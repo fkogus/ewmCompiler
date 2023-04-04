bfd0f1d8a9aa179697f1d6f18a2444d7ed559ad9
Felipe Kogus <felipekogus@gmail.com>
2023-04-04 10:13:02 -0300

    Fix codegen

--- codegen.py
+++ codegen.py
@@ -1,165 +1,153 @@
 import json
 
-def generate_header(header_data):
-    header_html = ''
+class CodeGenerator:
 
-    for item in header_data:
-        if 'def' in item:
-            if item['def'] == 'title':
-                header_html += f'<title>{item["arg"]}</title>\n'
-            elif item['def'] == 'lang':
-                header_html += f'<meta http-equiv="Content-Language" content="{item["arg"]}" />\n'
-            elif item['def'] == 'script':
-                header_html += f'<script src="{item["arg"]}" type="text/javascript"></script>\n'
-        elif 'style_def' in item:
-            for style_item in item['style_def']:
-                style_id = style_item["id"]
-                if style_id.startswith("$"):
-                    style_id = style_id[1:]
+    def __init__(self, ast):
+        self.ast = ast
+
+    def generate_header(self, header_data):
+        header_html = ''
+
+        for item in header_data:
+            if 'def' in item:
+                if item['def'] == 'title':
+                    header_html += f'<title>{item["arg"]}</title>\n'
+                elif item['def'] == 'lang':
+                    header_html += f'<meta http-equiv="Content-Language" content="{item["arg"]}" />\n'
+                elif item['def'] == 'script':
+                    header_html += f'<script src="{item["arg"]}" type="text/javascript"></script>\n'
+            elif 'style_def' in item:
+                for style_item in item['style_def']:
+                    style_id = style_item["id"]
+                    if style_id.startswith("$"):
+                        style_id = style_id[1:]
+                    else:
+                        style_id = "." + style_id
+                    style = ';'.join([f'{s["property"]}:{s["argument"]}' for s in style_item['style']])
+                    header_html += f'<style>{style_id}{{{style}}}</style>\n'
+
+        return header_html
+
+
+    def generate_container(self, c):
+
+        div_callback = False
+
+        container = ''
+
+        for item in c:
+
+            if 'container' in item:
+                container += self.generate_container(item['container'])
+                continue
+
+            if item['type'] == 'container':
+                container += "<div "
+                if 'class' in item:
+                    container += 'class = \"' + item['class'] + '\">\n'
                 else:
-                    style_id = "." + style_id
-                style = ';'.join([f'{s["property"]}:{s["argument"]}' for s in style_item['style']])
-                header_html += f'<style>{style_id}{{{style}}}</style>\n'
+                    container += ">\n"
+                div_callback = True
 
-    return header_html
+            elif item['type'] == 'component':
+                if item['command'] == 'img':
+                    container += '<img src=\"' + item['spec'][0] + '\" alt=\"' + item['spec'][1] + '\">\n'
 
+                elif item['command'] == 'input':
+                    container += '<input type=\"' + self.__get_input_type__(item['spec']['type']) + '\"> ' + item['spec']['arg'] + ' </input>\n'
+                
+                elif item['command'] == 'button':
+                    container += '<button type=\"' + item['spec']['type'] + '\"> ' + item['spec']['arg'] + ' </button>\n'
 
-def generate_container(c):
+                elif item['command'] == 'link':
+                    container += '<a href=\"' + item['spec'][0] + "\"> " + item['spec'][1] + ' </a>\n'
 
-    div_callback = False
+            elif item['type'] == 'form':
+                container += '<form action=\"' + item['arg'] + '\" method=\"' + item['method'] + '\">\n'
 
-    container = ''
+                container += self.generate_container(item['content'])
 
-    for item in c:
+                container += '\n</form>\n'
 
-        if 'container' in item:
-            container += generate_container(item['container'])
-            continue
+            elif item['type'] == 'text':
+                if 'class' in item:
+                    container += '<p class=\"' + item['class'] + '\">\n'
+                else:
+                    container += '<p>\n'
+                container += item['arg'] + '\n</p>'
 
-        if item['type'] == 'container':
-            container += "<div "
-            if 'class' in item:
-                container += 'class = \"' + item['class'] + '\">\n'
-            else:
-                container += ">\n"
-            div_callback = True
+            elif item['type'] == 'table':
+                container += self.generate_table(item)
 
-        elif item['type'] == 'component':
-            if item['command'] == 'img':
-                container += '<img src=\"' + item['spec'][0] + '\" alt=\"' + item['spec'][1] + '\">\n'
+        if div_callback:
+            container += '</div>'
 
-            elif item['command'] == 'input':
-                container += '<input type=\"' + __get_input_type__(item['spec']['type']) + '\"> ' + item['spec']['arg'] + ' </input>\n'
-            
-            elif item['command'] == 'button':
-                container += '<button type=\"' + item['spec']['type'] + '\"> ' + item['spec']['arg'] + ' </button>\n'
+        return container
 
-            elif item['command'] == 'link':
-                container += '<a href=\"' + item['spec'][0] + "\"> " + item['spec'][1] + ' </a>\n'
+    def generate_table(self, data):
+        table_html = '<table>'
 
-        elif item['type'] == 'form':
-            container += '<form action=\"' + item['arg'] + '\" method=\"' + item['method'] + '\">\n'
+        # Add table header to table
+        table_html += '<thead><tr>'
+        for header in data['thead']['values']:
+            table_html += f'<th>{header}</th>'
+        table_html += '</tr></thead>'
 
-            container += generate_container(item['content'])
+        # Add table body to table
+        table_html += '<tbody>'
+        for item in data['tbody']['items']:
+            table_html += '<tr>'
+            for value in item:
+                table_html += f'<td>{value}</td>'
+            table_html += '</tr>'
+        table_html += '</tbody>'
 
-            container += '\n</form>\n'
-
-        elif item['type'] == 'text':
-            if 'class' in item:
-                container += '<p class=\"' + item['class'] + '\">\n'
-            else:
-                container += '<p>\n'
-            container += item['arg'] + '\n</p>'
-
-        elif item['type'] == 'table':
-            container += generate_table(item)
-
-    if div_callback:
-        container += '</div>'
-
-    return container
-
-def generate_table(data):
-    table_html = '<table>'
-
-    # Add table header to table
-    table_html += '<thead><tr>'
-    for header in data['thead']['values']:
-        table_html += f'<th>{header}</th>'
-    table_html += '</tr></thead>'
-
-    # Add table body to table
-    table_html += '<tbody>'
-    for item in data['tbody']['items']:
-        table_html += '<tr>'
-        for value in item:
-            table_html += f'<td>{value}</td>'
-        table_html += '</tr>'
-    table_html += '</tbody>'
-
-    table_html += '</table>'
-    return table_html
+        table_html += '</table>'
+        return table_html
 
 
-def __get_input_type__(t):
-    if t == 'txt':
-        return 'text'
-    else:
-        return t
+    def __get_input_type__(self, t):
+        if t == 'txt':
+            return 'text'
+        else:
+            return t
     
+    def generate_html(self, output_file):
+        # Generate the header and CSS code
+        header_code = self.generate_header(self.ast[0]['header'])
 
-        
+        # Create a template HTML document
+        html_template = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          {header_code}
+          <style>body {{margin: 0;}}</style>
+        </head>
+        <body>
+          {page_content}
+        </body>
+        </html>
+        """
 
-import json
+        # Generate the page content
+        page = ''
+        page_json = self.ast[0]['page']['commands']
+        for a in page_json:
+            page += self.generate_container(a['container'])
 
-ast = {}
+        # Combine the generated code into the HTML document
+        html_document = html_template.format(
+            title=self.ast[0]['header'][0]['arg'],
+            header_code=header_code,
+            css_code='',
+            page_content=page
+        )
 
-with open('out.json', 'r') as file:
-    ast = json.load(file)
+        with open(output_file, "w") as outfile:
+            outfile.write(html_document)
 
-# Generate the header and CSS code
-header_code = generate_header(ast[0]['header'])
 
-# Create a template HTML document
-html_template = """
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  {header_code}
-  <style>body {{margin: 0;}}</style>
-</head>
-<body>
-  {page_content}
-</body>
-</html>
-"""
 
-# Insert the generated code into the HTML document
-# html_document = html_template.format(
-#     title=ast[0]['header'][0]['arg'],
-#     header_code=header_code,
-#     css_code=''
-# )
-
-page = ''
-
-page_json = ast[0]['page']['commands']#[0]['container']
-
-for a in page_json:
-    page += generate_container(a['container'])
-
-# page += generate_container(page_json)
-
-html_document = html_template.format(
-    title=ast[0]['header'][0]['arg'],
-    header_code=header_code,
-    css_code='',
-    page_content=page
-)
-
-# print(page_json)
-
-with open('teste.html', 'w') as teste:
-    print(html_document, file=teste)
 
